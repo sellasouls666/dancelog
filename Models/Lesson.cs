d4148c595e38097a4abbe3cb92b282df266afb7f
sellasouls666 <pgbc303@gmail.com>
2025-02-27 09:04:55 +0300

    Добавил класс Lesson

---
+++ Models/Lesson.cs
@@ -0,0 +1,9 @@
+﻿namespace dancelog.Models
+{
+    public class Lesson
+    {
+        public int Id { get; set; }
+        public Course Course { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
