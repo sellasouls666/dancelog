2603503ea4dc9ea46d33ed1d6a64b3f1ec978d86
sellasouls666 <pgbc303@gmail.com>
2025-02-27 08:58:10 +0300

    Сделал класс Group

---
+++ Models/Group.cs
@@ -0,0 +1,9 @@
+﻿namespace dancelog.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
