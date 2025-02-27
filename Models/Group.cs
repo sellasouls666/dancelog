0f56c4d772b2efa20472acb0f5d22b3d0ca731d7
sellasouls666 <pgbc303@gmail.com>
2025-02-27 09:01:25 +0300

    Доделал класс Group

--- Models/Group.cs
+++ Models/Group.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public Course Course { get; set; }
     }
 }
