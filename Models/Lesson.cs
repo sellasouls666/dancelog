4a1c9dea010bea6ce2c16f392090c747c2d3bbd9
sellasouls666 <pgbc303@gmail.com>
2025-02-27 09:07:35 +0300

    Доделал класс Lesson

--- Models/Lesson.cs
+++ Models/Lesson.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public Course Course { get; set; }
         public DateTime DateTime { get; set; }
+        public List<Attendance> AttendanceRecords { get; set; }
     }
 }
