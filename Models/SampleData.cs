e966c4e07da78eb5e7d7e4687893e375f39ce4ed
ziqara <165387888+ziqara@users.noreply.github.com>
2025-03-15 20:12:47 +0300

    Доработал класс Course, сделал класс для проверки страницы ListOfCourse.

---
+++ Models/SampleData.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using dancelog.Models;
+
+namespace dancelog.Data
+{
+    public class SampleData
+    {
+        public static List<Course> GetCourses()
+        {
+            return new List<Course>
+            {
+                new Course { Id = 1, Name = "Базовый курс бальных танцев" },
+                new Course { Id = 2, Name = "Хип-хоп для начинающих" },
+                new Course { Id = 3, Name = "Современная хореография" },
+                new Course { Id = 4, Name = "Танго: страсть и элегантность" },
+                new Course { Id = 5, Name = "Джаз-модерн" },
+                new Course { Id = 6, Name = "Сальса и бачата" },
+                new Course { Id = 7, Name = "Классический балет" },
+                new Course { Id = 8, Name = "Восточные танцы" },
+                new Course { Id = 9, Name = "Брейк-данс" },
+                new Course { Id = 10, Name = "Актерское мастерство в танце" }
+            };
+        }
+    }
+}
+
--- Pages/ListOfCourse.cshtml.cs
+++ Pages/ListOfCourse.cshtml.cs
@@ -1,3 +1,5 @@
+using dancelog.Data;
+using dancelog.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,11 @@ namespace dancelog.Pages
 {
     public class ListOfCourseModel : PageModel
     {
+
+        public List<Course> Courses { get; set; } = new List<Course>();
         public void OnGet()
         {
+            Courses = SampleData.GetCourses();
         }
     }
 }
