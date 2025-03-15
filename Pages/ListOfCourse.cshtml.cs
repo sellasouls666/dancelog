9dc68d374dd70d0a9b3d2b9d9c17ce9a58762eda
ziqara <165387888+ziqara@users.noreply.github.com>
2025-03-15 20:05:02 +0300

    Создал пустую страницу Razor, указал её в Layot

---
+++ Pages/ListOfCourse.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dancelog.Pages
+{
+    public class ListOfCourseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
