90c335f68fc244db1c7731e2f5de419d4c240f2d
meow <damirzhum345@gmail.com>
2025-04-04 09:20:07 +0300

    Попытался зафиксить баг с пулом

--- Pages/ListOfGroups.cshtml.cs
+++ Pages/ListOfGroups.cshtml.cs
@@ -1,17 +1,24 @@
 using dancelog.Data;
 using dancelog.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace dancelog.Pages
 {
     public class ListOfGroupsModel : PageModel
     {
-        public List<Group> Groups { get; set; } = new List<Group>();
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public ListOfGroupsModel(ApplicationDbContext context)
         {
-            
+            _context = context;
         }
 
+        public IList<Group> Groups { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Groups = await _context.Groups.Include(g => g.Course).ToListAsync();
+        }
     }
 }
