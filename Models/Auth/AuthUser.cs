5ac6a4361cc65f191a87ed6664405ab6ad0b57db
ziqara <165387888+ziqara@users.noreply.github.com>
2025-04-13 00:21:45 +0300

    Сделал модель для будущей авторизации

---
+++ Models/Auth/AuthUser.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dancelog.Models.Auth
+{
+    public class AuthUser
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Не указан Email")]
+        [EmailAddress(ErrorMessage = "Некорректный Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Роль обязательна")]
+        public string Role { get; set; }
+
+        [Required(ErrorMessage = "Не указана фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя")]
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        // Вычисляемое свойство для полного имени
+        public string FullName => $"{LastName} {FirstName}{(string.IsNullOrWhiteSpace(MiddleName) ? "" : " " + MiddleName)}";
+    }
+}
---
+++ Models/Auth/RegisterModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dancelog.Models.Auth
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Не указана фамилия")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Не указано имя")]
+        public string FirstName { get; set; }
+
+        // Отчество можно оставить необязательным
+        public string MiddleName { get; set; }
+
+        [Required(ErrorMessage = "Не указан Email")]
+        [EmailAddress(ErrorMessage = "Некорректный Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Необходимо подтвердить пароль")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
