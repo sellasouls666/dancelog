94c64f90d986781e90abd023e30b4f92f9a4d3bf
ziqara <165387888+ziqara@users.noreply.github.com>
2025-04-14 01:31:38 +0300

    Страница listofusers выдает всех пользователей в виде таблицы.

--- Pages/Account/ListOfUsers.cshtml.cs
+++ Pages/Account/ListOfUsers.cshtml.cs
@@ -1,12 +1,24 @@
-using Microsoft.AspNetCore.Mvc;
+using dancelog.Data;
+using dancelog.Models.Auth;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace dancelog.Pages.Account
 {
     public class ListOfUsersModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public ListOfUsersModel(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public IList<AuthUser> Users { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Users = await _context.AuthUsers.ToListAsync();
         }
     }
 }
