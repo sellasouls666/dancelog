357c52f3958a8c056192fafd1672d7a40695e9eb
ziqara <165387888+ziqara@users.noreply.github.com>
2025-04-14 01:16:20 +0300

    Страница пользователя появляется только у пользователя с ролью "Админ"

---
+++ Pages/Account/ListOfUsers.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dancelog.Pages.Account
+{
+    public class ListOfUsersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
