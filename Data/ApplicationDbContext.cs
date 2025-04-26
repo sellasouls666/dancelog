d77fbcd33e718074d704dd96c619aeb00fc71ee9
sellasouls666 <pgbc303@gmail.com>
2025-04-26 12:21:34 +0300

    Сделал unit тест на добавление курса

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -13,7 +13,7 @@ namespace dancelog.Data
             }
 
             public DbSet<Student> Students { get; set; }
-            public DbSet<Course> Courses { get; set; }
+            public virtual DbSet<Course> Courses { get; set; }
             public DbSet<Group> Groups { get; set; }
             public DbSet<Lesson> Lessons { get; set; }
             public DbSet<Attendance> AttendanceRecords { get; set; }
