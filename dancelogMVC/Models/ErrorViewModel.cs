66bcef36c2b7c5ac5719539f2b8f83938b1ceaf1
wormgod <pgbc303@gmail.com>
2025-05-12 19:18:34 +0300

    Откат

--- dancelogMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace dancelogMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
