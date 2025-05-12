2c9f379ec83cec338e2d66a2e5d7e0e4e0bb96d2
wormgod <pgbc303@gmail.com>
2025-05-12 19:17:54 +0300

    откат

---
+++ dancelogMVC/Controllers/CourseApiController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using dancelogMVC.Models;
+
+namespace dancelogMVC.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoursesApiController : Controller
+    {
+        private static readonly List<Course> courses = new()
+        {
+            new Course { Id = 1, Name = "Test" }
+        };
+
+        [HttpGet]
+        public IActionResult Get() => Ok(courses);
+
+        [HttpPost]
+        public IActionResult Post(Course course)
+        {
+            course.Id = courses.Max(c => c.Id) + 1;
+            courses.Add(course);
+            return CreatedAtAction(nameof(Get), new { id = course.Id }, course);
+        }
+    }
+}
---
+++ dancelogMVC/Controllers/CourseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using dancelogMVC.Models;
+
+namespace dancelogMVC.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var courses = new List<Course> {
+            new Course { Id = 1, Name = "Test" }};
+
+            return View(courses);
+        }
+    }
+}
