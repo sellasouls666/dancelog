d8f74493c916e158af13cdda27c7af894ce712a7
wormgod <pgbc303@gmail.com>
2025-05-15 16:17:28 +0300

    Сделал тесты на валидацию моделей Lesson и Student

---
+++ dancelog.Tests/Unit/LessonValidationTests.cs
@@ -0,0 +1,86 @@
+﻿using dancelog.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dancelog.Tests.Unit
+{
+    public class LessonValidationTests
+    {
+        [Fact]
+        public void Lesson_WithValidData_ShouldBeValid()
+        {
+            var course = new Course
+            {
+                Id = 0,
+                Name = "3"
+            };
+
+            var group = new Group
+            {
+                Id = 0,
+                Name = "П-30",
+                CourseId = course.Id,
+                Course = course
+            };
+
+            var lesson = new Lesson
+            {
+                Id = 0,
+                Name = "MDK 01.01",
+                GroupId = group.Id,
+                Group = group,
+                DateTime = DateTime.Now
+            };
+
+            var context = new ValidationContext(lesson);
+
+            var result = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(lesson, context, result, true);
+
+            Assert.True(isValid);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Lesson_WithEmptyName_ShouldBeInvalid()
+        {
+            var course = new Course
+            {
+                Id = 0,
+                Name = "3"
+            };
+
+            var group = new Group
+            {
+                Id = 0,
+                Name = "П-30",
+                CourseId = course.Id,
+                Course = course
+            };
+
+            var lesson = new Lesson
+            {
+                Id = 0,
+                Name = "",
+                GroupId = group.Id,
+                Group = group,
+                DateTime = DateTime.Now
+            };
+
+            var context = new ValidationContext(lesson);
+            var result = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(lesson, context, result, true);
+
+            Assert.False(isValid);
+
+            Assert.Contains(result, r => r.ErrorMessage == "Необходимо указать название урока");
+        }
+    }
+}
---
+++ dancelog.Tests/Unit/StudentValidationTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dancelog.Models;
+
+namespace dancelog.Tests.Unit
+{
+    public class StudentValidationTests
+    {
+        [Fact]
+        public void Student_WithValidData_ShouldBeValid()
+        {
+            var course = new Course
+            {
+                Id = 0,
+                Name = "3"
+            };
+
+            var group = new Group
+            {
+                Id = 0,
+                Name = "П-30",
+                CourseId = course.Id,
+                Course = course
+            };
+
+            var student = new Student
+            {
+                Id = 0,
+                Surname = "Глагольев",
+                Name = "Егор",
+                Patronymic = "Викторович",
+                GroupId = group.Id,
+                Group = group,
+                Email = "pgbc303@gmail.com"
+            };
+
+            var context = new ValidationContext(student);
+
+            var result = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(student, context, result, true);
+
+            Assert.True(isValid);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Student_WithEmptySurname_ShouldBeInvalid()
+        {
+            var course = new Course
+            {
+                Id = 0,
+                Name = "3"
+            };
+
+            var group = new Group
+            {
+                Id = 0,
+                Name = "П-30",
+                CourseId = course.Id,
+                Course = course
+            };
+
+            var student = new Student
+            {
+                Id = 0,
+                Surname = "",
+                Name = "Егор",
+                Patronymic = "Викторович",
+                GroupId = group.Id,
+                Group = group,
+                Email = "pgbc303@gmail.com"
+            };
+
+            var context = new ValidationContext(student);
+            var result = new List<ValidationResult>();
+
+            var isValid = Validator.TryValidateObject(student, context, result, true);
+
+            Assert.False(isValid);
+
+            Assert.Contains(result, r => r.ErrorMessage == "Поле обязательно для заполнения");
+        }
+    }
+}
